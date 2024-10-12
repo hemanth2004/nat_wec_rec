f07a0cb50b13969fce26a76e5180548ff75b4cb8
Hemanth Srinivash Elangovan <49274253+hemanth2004@users.noreply.github.com>
2024-10-12 22:40:56 +0530

    Update vnet.sh to now have packet filter working

--- vnet.sh
+++ vnet.sh
@@ -5,7 +5,9 @@
 # This network exists bebtween the router and the clients like a typical star topology LAN setup
 
 
-# enable packet forwarding
+# preparing kernel for the network
+ip -all netns delete
+iptables -F
 sysctl -w net.ipv4.ip_forward=1
 
 
@@ -13,13 +15,18 @@ sysctl -w net.ipv4.ip_forward=1
 ip netns add router
 ip -n router link set lo up
 
+
+
 # link up the router and the root namespace
+# 192.168.11.1 could be regarded as the router's "public IP" in this context
 ip link add veth-root type veth peer name veth-router
 ip link set veth-router netns router
 ip addr add 192.168.11.2/24 dev veth-root
 ip -n router addr add 192.168.11.1/24 dev veth-router
 ip link set veth-root up
 ip -n router link set veth-router up
+
+
 
 # to make veth-router act as an external connection to the internet 
 # (taking root namespace as part of the 'internet')
@@ -29,15 +36,31 @@ iptables -t nat -A POSTROUTING -s 192.168.11.0/24 -o enp0s3 -j MASQUERADE
 iptables -A FORWARD -i enp0s3 -o veth-root -j ACCEPT
 iptables -A FORWARD -o enp0s3 -i veth-root -j ACCEPT
 
+
+
 # set up virtual switch of router
 ip -n router link add vbr type bridge
 ip -n router addr add 192.168.10.1/24 dev vbr
 ip -n router link set vbr up
 
+
+
 # the actual router NATting that concerns our 192.168.10.0 network
+# also implement the firewall here
+ip netns exec router iptables -F
+ip netns exec router iptables -t nat -F
 ip netns exec router iptables -t nat -A POSTROUTING -s 192.168.10.0/24 -o veth-router -j MASQUERADE
-ip netns exec router iptables -A FORWARD -i veth-router -o vbr -j ACCEPT
-ip netns exec router iptables -A FORWARD -i vbr -o veth-router -j ACCEPT
+
+# allow outgoing port 80 and 443 traffic
+ip netns exec router iptables -A FORWARD -i vbr -o veth-router -p tcp --dport 80 -j ACCEPT
+ip netns exec router iptables -A FORWARD -i vbr -o veth-router -p tcp --dport 443 -j ACCEPT
+
+# allow all incoming traffic since the firewall is only for outbound traffic
+ip netns exec router iptables -A FORWARD -i veth-router -o vbr -p tcp -j ACCEPT
+
+# once filters are specified, drop all packets
+ip netns exec router iptables -P FORWARD DROP
+
 
 
 # set up client1
@@ -49,6 +72,7 @@ ip -n router link set veth1 netns client1
 ip -n client1 link set veth1 up
 ip -n client1 addr add 192.168.10.101/24 dev veth1
 ip -n client1 route add default via 192.168.10.1
+
 
 
 # set up client2
@@ -65,14 +89,26 @@ ip -n client2 route add default via 192.168.10.1
 ip -n router link set vbr up
 
 
-# forward port 80 requests on client1 to client1's web server
+# creation of a http web server on client1
+ip netns exec client1 python3 -m http.server 80 &
+
+sleep 2
+
+# forward port 80 requests on network's "public IP" to client1's web server
 ip netns exec router iptables -t nat -A PREROUTING -d 192.168.11.1 -p tcp --dport 80 -j DNAT --to-destination 192.168.10.101:80
 ip netns exec router iptables -A FORWARD -p tcp -d 192.168.10.101 --dport 80 -j ACCEPT
 
-# explicitly stop ICMP packets including ping services
-ip netns exec router iptables -A FORWARD -p icmp -j DROP
 
-# allow only http (80) and https (443) outgoing packets
-ip netns exec router iptables -A FORWARD -j DROP
-ip netns exec router iptables -A FORWARD -p tcp --dport 80 -j ACCEPT
-ip netns exec router iptables -A FORWARD -p tcp --dport 443 -j ACCEPT
+
+
+echo ' '
+echo 'Testing http from client1'
+ip netns exec client1 curl http://93.184.216.34
+echo ' '
+
+echo 'Testing https from client2'
+ip netns exec client2 curl -k https://104.16.132.229
+echo ' '
+
+echo 'Testing ping 8.8.8.8 from client1'
+ip netns exec client1 ping 8.8.8.8
